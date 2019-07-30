bea23b7ab4628ddafd3ede94ac33d7a053c8eba4
Unknown <guenin.vincent4@gmail.com>
2019-07-30 14:34:42 +0200

    [UPD] Implem w/ stream

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -5,15 +5,7 @@ class IsogramChecker {
     boolean isIsogram(String phrase) {
         phrase = phrase.replace("-", "").replace(" ", "").toLowerCase();
 
-        List<String> characters = Arrays.asList(phrase.split(""));
-        Set<String> characterCounter = new HashSet<>(characters);
-        for (String character: characterCounter) {
-            if (Collections.frequency(characters, character) > 1) {
-                return false;
-            }
-        }
-
-        return true;
+        return phrase.chars().distinct().count() == phrase.length();
     }
 
 }
