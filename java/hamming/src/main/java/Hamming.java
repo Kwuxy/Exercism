9b32db5b11429b7de0a9647c3afcce49146e4273
Unknown <guenin.vincent4@gmail.com>
2019-07-31 10:07:08 +0200

    Setup

---
+++ java/hamming/src/main/java/Hamming.java
@@ -0,0 +1,14 @@
+class Hamming {
+    private final String leftStrand;
+    private final String rightStrand;
+
+    Hamming(String leftStrand, String rightStrand) {
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
+    }
+
+    int getHammingDistance() {
+        return 0;
+    }
+
+}
