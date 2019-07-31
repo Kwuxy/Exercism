6d3591795ae8983ff95dd994028cc995e88e2d9e
Unknown <guenin.vincent4@gmail.com>
2019-07-31 14:55:42 +0200

    Implem

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,19 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PangramChecker {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        final List<String> alphabet = Arrays.asList("abcdefghijklmnopqrstuvwxyz".split(""));
+
+        return input.toLowerCase()
+                .chars()
+                .mapToObj(c -> (char) c)
+                .map(Object::toString)
+                .collect(Collectors.toCollection(HashSet::new))
+                .containsAll(alphabet);
     }
 
 }
