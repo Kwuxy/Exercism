192bf00561eb03bd9abcc4c79f7218d6abd77d31
Unknown <guenin.vincent4@gmail.com>
2019-07-31 15:20:04 +0200

    Implem

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,22 @@
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder builder = new StringBuilder();
+
+        for (String part: dnaStrand.split("")) {
+            builder.append(transcribeSingle(part));
+        }
+
+        return builder.toString();
+    }
+
+    String transcribeSingle(String dnaStrand) {
+        if (dnaStrand.equals("C")) return "G";
+        if (dnaStrand.equals("G")) return "C";
+        if (dnaStrand.equals("T")) return "A";
+        if (dnaStrand.equals("A")) return "U";
+
+        return "";
     }
 
 }
