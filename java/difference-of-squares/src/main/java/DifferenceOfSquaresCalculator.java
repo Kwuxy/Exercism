2deced6d44ed809cf55945db64aa12193ee64080
Vincent Guénin <guenin.vincent4@gmail.com>
2019-09-03 18:09:59 +0200

    Implem

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,20 @@
+import java.util.stream.IntStream;
+
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return computeSquare(IntStream.range(1, input + 1).sum());
     }
 
     int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return IntStream.range(1, input + 1).map(this::computeSquare).sum();
+    }
+
+    private int computeSquare(int input) {
+        return (int) Math.pow(input, 2);
     }
 
     int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
     }
-
 }
