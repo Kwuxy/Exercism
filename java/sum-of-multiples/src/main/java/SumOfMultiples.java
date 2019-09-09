a3bae6574169704eca24b30292978c0227ff49d2
Vincent Guénin <guenin.vincent4@gmail.com>
2019-09-09 12:30:36 +0200

    Implem

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,11 +1,30 @@
+import java.util.stream.IntStream;
+
 class SumOfMultiples {
 
+    private final int[] set;
+    private final int limit;
+
     SumOfMultiples(int number, int[] set) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.set = cleanSet(set);
+        this.limit = number;
+    }
+
+    private int[] cleanSet(int[] set) {
+        return IntStream.of(set)
+                .filter(value -> value != 0)
+                .toArray();
     }
 
     int getSum() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return IntStream.range(1, limit)
+                .filter(this::isMultipleOfSet)
+                .sum();
+    }
+
+    private boolean isMultipleOfSet(int i) {
+        return IntStream.of(set)
+                .anyMatch(value -> i % value == 0);
     }
 
 }
