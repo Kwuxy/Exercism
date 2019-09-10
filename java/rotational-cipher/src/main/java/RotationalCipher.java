5dd28788a506368057fc30ea5073e43ee1645f4a
Vincent Guénin <guenin.vincent4@gmail.com>
2019-09-10 17:51:56 +0200

    Implem

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,11 +1,30 @@
 class RotationalCipher {
+    private final int shiftKey;
 
     RotationalCipher(int shiftKey) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.shiftKey = shiftKey;
     }
 
     String rotate(String data) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return data.chars()
+                .map(this::rotateSingleCharacter)
+                .collect(StringBuilder::new,
+                        StringBuilder::appendCodePoint,
+                        StringBuilder::append)
+                .toString();
     }
 
+    private char rotateSingleCharacter(int character) {
+        if(Character.isLowerCase(character)) {
+            return rotateSingleCharacter(character, 'a');
+        } else if(Character.isUpperCase(character)) {
+            return rotateSingleCharacter(character, 'A');
+        }
+
+        return (char) character;
+    }
+
+    private char rotateSingleCharacter(int character, int reference) {
+        return (char) ((character - reference + shiftKey) % 26 + reference);
+    }
 }
