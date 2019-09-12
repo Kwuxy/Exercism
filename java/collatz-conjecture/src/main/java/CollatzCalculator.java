bb0805085723cde09000e0e3521907723e794426
Vincent Guénin <guenin.vincent4@gmail.com>
2019-09-12 12:12:24 +0200

    Implem

--- java/collatz-conjecture/src/main/java/CollatzCalculator.java
+++ java/collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -1,7 +1,30 @@
 class CollatzCalculator {
 
     int computeStepCount(int start) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if(!isValid(start)) throw new IllegalArgumentException("Only natural numbers are allowed");
+        if(start == 1) return 0;
+
+        int modifiedValue = computeStep(start);
+        return computeStepCount(modifiedValue) + 1;
     }
 
+    private int computeStep(int value) {
+        if(value % 2 == 0) {
+            return EvenCase(value);
+        } else {
+            return OddCase(value);
+        }
+    }
+
+    private int EvenCase(int value) {
+        return value / 2;
+    }
+
+    private int OddCase(int value) {
+        return value * 3 + 1;
+    }
+
+    private boolean isValid(int value) {
+        return value > 0;
+    }
 }
