76ae357416616a4b48d6e0b26d21f35344774c19
Vincent Guénin <guenin.vincent4@gmail.com>
2019-10-04 09:26:12 +0200

    Implem

--- java/nucleotide-count/src/main/java/NucleotideCounter.java
+++ java/nucleotide-count/src/main/java/NucleotideCounter.java
@@ -1,10 +1,30 @@
-/*
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class NucleotideCounter {
 
-Please remove this comment when submitting your solution.
+    private final Map<Character, Integer> occurrence = Stream.of(new Object[]{'A', 'C', 'G', 'T'})
+            .collect(Collectors.toMap(nucleotide -> (Character) nucleotide, nucleotide -> 0));
 
-*/
+    public NucleotideCounter(String dna) {
+        count(dna);
+    }
+
+    private void count(String dna) {
+        dna.chars().forEach(nucleotide -> incrementNucleotide((char) nucleotide));
+    }
+
+    private void incrementNucleotide(Character nucleotide) {
+        final Integer occ = occurrence.get(nucleotide);
+        if(occ == null) {
+            throw new IllegalArgumentException();
+        }
+
+        occurrence.replace(nucleotide, occ + 1);
+    }
+
+    public Map<Character, Integer> nucleotideCounts() {
+        return occurrence;
+    }
+}
