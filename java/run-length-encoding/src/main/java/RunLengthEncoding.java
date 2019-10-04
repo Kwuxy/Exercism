2bdd169a800f607e5f3398d0c4d24428a2305bcc
Vincent Guénin <guenin.vincent4@gmail.com>
2019-10-04 20:01:01 +0200

    Implem

--- java/run-length-encoding/src/main/java/RunLengthEncoding.java
+++ java/run-length-encoding/src/main/java/RunLengthEncoding.java
@@ -1,10 +1,31 @@
-/*
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class RunLengthEncoding {
+    public String encode(String text) {
+        return Arrays.stream(text.split("(?<=([\\w\\s]))(?!\\1)"))
+                .map(this::buildEncodedPart)
+                .collect(Collectors.joining());
+    }
 
-Please remove this comment when submitting your solution.
+    private String buildEncodedPart(String s) {
+        if(s.length() < 2) return s;
+        return String.valueOf(s.length()) + s.charAt(0);
+    }
 
-*/
+    public String decode(String encoded) {
+        return Arrays.stream(encoded.split("(?<=([a-zA-Z\\s]))"))
+                .map(this::buildDecodedPart)
+                .collect(Collectors.joining());
+    }
+
+    private String buildDecodedPart(String s) {
+        if(s.length() < 2) return s;
+
+        final String[] splitString = s.split("[a-zA-Z\\s]");
+        final int occurrence = splitString[0].length() == 0 ? 1 : Integer.valueOf(splitString[0]);
+        final char character = s.charAt(s.length() - 1);
+
+        return String.valueOf(character).repeat(occurrence);
+    }
+}
