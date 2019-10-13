668fb56925b37fb5122d154038a7cb6f7bf82363
Vincent Guénin <guenin.vincent4@gmail.com>
2019-10-13 22:08:08 +0200

    Implem

--- java/resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ java/resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+
 class ResistorColorDuo {
+    private static final List colors = Arrays.asList("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white");
+
+    private int getValueOf(String color) {
+        return colors.indexOf(color);
+    }
+
     int value(String[] colors) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return Arrays.stream(cutArrayToUsefulData(colors))
+                .map(this::getValueOf)
+                .reduce(0, this::concatNumbers);
+    }
+
+    private String[] cutArrayToUsefulData(String[] colors) {
+        return Arrays.copyOfRange(colors, 0, 2);
+    }
+
+    private int concatNumbers(int a, int b) {
+        return a * 10 + b;
     }
 }
