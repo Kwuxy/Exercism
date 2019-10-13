268c37514f2b7a5ed63809a3b1bf236d05adaa56
Vincent Guénin <guenin.vincent4@gmail.com>
2019-10-13 22:34:45 +0200

    Implem

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -1,13 +1,21 @@
 import java.math.BigInteger;
+import java.util.stream.IntStream;
 
 class Grains {
 
     BigInteger computeNumberOfGrainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if(!isValid(square)) throw new IllegalArgumentException("square must be between 1 and 64");
+
+        return new BigInteger("1").shiftLeft(square - 1);
     }
 
     BigInteger computeTotalNumberOfGrainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return IntStream.rangeClosed(1, 64)
+                .mapToObj(this::computeNumberOfGrainsOnSquare)
+                .reduce(new BigInteger("0"), BigInteger::add);
     }
 
+    private boolean isValid(final int square) {
+        return square > 0 && square < 65;
+    }
 }
