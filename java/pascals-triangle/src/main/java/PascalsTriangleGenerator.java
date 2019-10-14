53304f83c92551b08fc421f7b648c35cbc79fd5b
Vincent Guénin <guenin.vincent4@gmail.com>
2019-10-14 19:09:00 +0200

    Implem

--- java/pascals-triangle/src/main/java/PascalsTriangleGenerator.java
+++ java/pascals-triangle/src/main/java/PascalsTriangleGenerator.java
@@ -1,10 +1,26 @@
-/*
+import java.util.stream.IntStream;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class PascalsTriangleGenerator {
 
-Please remove this comment when submitting your solution.
+    public int[][] generateTriangle(int lines) {
+        return IntStream.rangeClosed(1, lines)
+                .mapToObj(this::generateTriangleLine)
+                .toArray(int[][]::new);
+    }
 
-*/
+    private int[] generateTriangleLine(int line) {
+        int[] result = initLine(line);
+
+        for(int i = 1; i < line; i++) {
+            result[i] = result[i - 1] * (line - i) / i;
+        }
+
+        return result;
+    }
+
+    private int[] initLine(int size) {
+        int[] line = new int[size];
+        line[0] = 1;
+        return line;
+    }
+}
