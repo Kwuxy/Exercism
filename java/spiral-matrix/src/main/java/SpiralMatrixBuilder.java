1fb23fb3269e7440730145b38090f0cbe10c7ad7
Vincent Guénin <guenin.vincent4@gmail.com>
2019-10-23 14:35:12 +0200

    Implem

--- java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
+++ java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
@@ -1,10 +1,41 @@
-/*
+class SpiralMatrixBuilder {
+    private int size;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    public int[][] buildMatrixOfSize(int size) {
+        this.size = size;
+        final int[][] matrix = generateMatrix();
+        return calculateAllMatrixCellValue(matrix);
+    }
 
-Please remove this comment when submitting your solution.
+    private int[][] calculateAllMatrixCellValue(int[][] matrix) {
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                matrix[row][col] = calculateMatrixCellValue(row, col);
+            }
+        }
 
-*/
+        return matrix;
+    }
+
+    private int[][] generateMatrix() {
+        return new int[size][size];
+    }
+
+    private int calculateMatrixCellValue(int row, int col) {
+        return row <= col ? calculateMatrixCellValueTopRight(row, col) : calculateMatrixCellValueBottomLeft(row, col);
+    }
+
+    private int calculateMatrixCellValueBottomLeft(int row, int col) {
+        int x = calculateConcentricCercle(row, col);
+        return (size * size) - ((size - 2 * x - 2) * (size - 2 * x - 2) + (row - x) + (col - x)) + 1;
+    }
+
+    private int calculateMatrixCellValueTopRight(int row, int col) {
+        int x = calculateConcentricCercle(row, col);
+        return (size * size) - ((size - 2 * x) * (size - 2 * x) - (col - x) - (row - x)) + 1;
+    }
+
+    private int calculateConcentricCercle(int row, int col) {
+        return Math.min(Math.min(row, col), Math.min(size - row - 1, size - col - 1));
+    }
+}
