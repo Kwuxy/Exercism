db8cf104f8b4386ef239d2c063803e6079f467fb
Vincent Guénin <guenin.vincent4@gmail.com>
2019-10-25 18:37:03 +0200

    Implem

--- java/beer-song/src/main/java/BeerSong.java
+++ java/beer-song/src/main/java/BeerSong.java
@@ -1,10 +1,30 @@
-/*
+class BeerSong {
+    public String sing(int bottlesNumber, int beerToDrink) {
+        final StringBuilder builder = new StringBuilder();
+        while(beerToDrink > 0) {
+            builder.append(getPhrase(bottlesNumber));
+            beerToDrink--;
+            bottlesNumber--;
+        }
+        return builder.toString();
+    }
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    private String getPhrase(int bottlesNumber) {
+        switch(bottlesNumber) {
+            case 0: return "No more bottles of beer on the wall, no more bottles of beer.\n" +
+                                    "Go to the store and buy some more, 99 bottles of beer on the wall.\n\n";
+            case 1: return "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                                    "Take it down and pass it around, no more bottles of beer on the wall.\n\n";
+            case 2: return "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+                                    "Take one down and pass it around, 1 bottle of beer on the wall.\n\n";
+            default:
+                String pluralPhrase = "%d bottles of beer on the wall, %d bottles of beer.\n" +
+                        "Take one down and pass it around, %d bottles of beer on the wall.\n\n";
+                return String.format(pluralPhrase, bottlesNumber, bottlesNumber, bottlesNumber - 1);
+        }
+    }
 
-Please remove this comment when submitting your solution.
-
-*/
+    public String singSong() {
+        return sing(99, 100);
+    }
+}
