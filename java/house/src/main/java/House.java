b7c69b90277b19acb670976f5ba143f7afe8c92b
Vincent Guénin <guenin.vincent4@gmail.com>
2019-10-25 12:00:44 +0200

    Implem

--- java/house/src/main/java/House.java
+++ java/house/src/main/java/House.java
@@ -1,10 +1,39 @@
-/*
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class House {
 
-Please remove this comment when submitting your solution.
+    private List<String> verseParts = Stream.of(new String[] {
+            "the house that Jack built.",
+            "the malt that lay in ",
+            "the rat that ate ",
+            "the cat that killed ",
+            "the dog that worried ",
+            "the cow with the crumpled horn that tossed ",
+            "the maiden all forlorn that milked ",
+            "the man all tattered and torn that kissed ",
+            "the priest all shaven and shorn that married ",
+            "the rooster that crowed in the morn that woke ",
+            "the farmer sowing his corn that kept ",
+            "the horse and the hound and the horn that belonged to ",
+    }).collect(Collectors.toList());
 
-*/
+    public String verse(int verse) {
+        return "This is " + IntStream.range(0, verse)
+                .map(i -> verse - i - 1)
+                .mapToObj(verseNumber -> verseParts.get(verseNumber))
+                .collect(Collectors.joining());
+    }
+
+    public String verses(int startVerse, int endVerse) {
+        return IntStream.rangeClosed(startVerse, endVerse)
+                .mapToObj(this::verse)
+                .collect(Collectors.joining("\n"));
+    }
+
+    public String sing() {
+        return verses(1, 12);
+    }
+}
