e9b0149f2c74a1c0767c50abe112129fdc89740a
Vincent Guénin <guenin.vincent4@gmail.com>
2019-10-25 18:03:11 +0200

    Implem

--- java/food-chain/src/main/java/FoodChain.java
+++ java/food-chain/src/main/java/FoodChain.java
@@ -1,10 +1,49 @@
-/*
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class FoodChain {
+    private List<String[]> parts = Stream.of(new String[][] {
+            {"fly.", "", ""},
+            {"", "spider.\nIt ", "wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the "},
+            {"bird.\n", "How absurd to swallow a bird!\n", "She swallowed the bird to catch the spider that "},
+            {"cat.\n", "Imagine that, to swallow a cat!\n", "She swallowed the cat to catch the "},
+            {"dog.\n", "What a hog, to swallow a dog!\n", "She swallowed the dog to catch the "},
+            {"goat.\n", "Just opened her throat and swallowed a goat!\n", "She swallowed the goat to catch the "},
+            {"cow.\nI don't know how she swallowed a cow!\nShe swallowed the cow to catch the ", "", ""},
+    }).collect(Collectors.toList());
 
-Please remove this comment when submitting your solution.
+    private int verse;
 
-*/
+    public String verse(int verse) {
+        this.verse = verse;
+        String beginPhrase = "I know an old lady who swallowed a ";
+        String endPhrase = "\nI don't know why she swallowed the fly. Perhaps she'll die.";
+        String horsePhrase = "horse.\nShe's dead, of course!";
+        if(verse == 8) return beginPhrase + horsePhrase;
+
+        return beginPhrase + generateVerseBody() + endPhrase;
+    }
+
+    private String generateVerseBody() {
+        return IntStream.range(0, verse)
+                .map(verseNumber -> verse - verseNumber - 1)
+                .mapToObj(this::generateVersePart)
+                .collect(Collectors.joining(""));
+    }
+
+    private String generateVersePart(int verseNumber) {
+        String result = parts.get(verseNumber)[0];
+        if(verse - 1 == verseNumber) {
+            result += parts.get(verseNumber)[1];
+        }
+        return result + parts.get(verseNumber)[2];
+    }
+
+    public String verses(int startVerse, int endVerse) {
+        return IntStream.rangeClosed(startVerse, endVerse)
+                .mapToObj(this::verse)
+                .collect(Collectors.joining("\n\n"));
+    }
+}
