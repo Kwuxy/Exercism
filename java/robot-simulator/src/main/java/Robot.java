73bc77b79607a0c5260ff5dcce3703e73c16e129
Vincent Guénin <guenin.vincent4@gmail.com>
2019-11-02 10:33:31 +0100

    Implem

--- java/robot-simulator/src/main/java/Robot.java
+++ java/robot-simulator/src/main/java/Robot.java
@@ -1,10 +1,72 @@
-/*
+import java.util.Arrays;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class Robot {
 
-Please remove this comment when submitting your solution.
+    private GridPosition gridPosition;
+    private Orientation orientation;
 
-*/
+    public Robot(GridPosition gridPosition, Orientation orientation) {
+        this.gridPosition = gridPosition;
+        this.orientation = orientation;
+    }
+
+    public Orientation getOrientation() {
+        return this.orientation;
+    }
+
+    public GridPosition getGridPosition() {
+        return this.gridPosition;
+    }
+
+    public void turnRight() {
+        final int ordinal = getOrientationOrdinal();
+        this.orientation = Orientation.values()[(ordinal + 1) % 4];
+    }
+
+    public void turnLeft() {
+        final int ordinal = getOrientationOrdinal();
+        this.orientation = Orientation.values()[(ordinal + 4 - 1) % 4];
+    }
+
+    private int getOrientationOrdinal() {
+        return Orientation.valueOf(this.orientation.toString()).ordinal();
+    }
+
+    public void advance() {
+        this.gridPosition = moveForward();
+    }
+
+    private GridPosition moveForward() {
+        int x = this.gridPosition.x;
+        int y = this.gridPosition.y;
+
+        switch(this.orientation) {
+            case NORTH:
+                y++; break;
+            case SOUTH:
+                y--; break;
+            case EAST:
+                x++; break;
+            case WEST:
+                x--; break;
+        }
+
+        return new GridPosition(x, y);
+    }
+
+    public void simulate(String sequence) {
+        Arrays.stream(sequence.split(""))
+                .forEach(this::executeAction);
+    }
+
+    private void executeAction(String action) {
+        switch(action) {
+            case "R":
+                turnRight(); break;
+            case "L":
+                turnLeft(); break;
+            case "A":
+                advance(); break;
+        }
+    }
+}
